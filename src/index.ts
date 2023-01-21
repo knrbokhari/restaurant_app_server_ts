579ec12f6dd3e5610f3bab0d4d136eb810d60f19
kazi naeem rayhan <kazinaeemrayhan@gmail.com>
2023-01-21 19:07:45 +0600

    mongoDB connected

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import app from './app';
+import connectDB from './connectDB';
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
+  connectDB()
   /* eslint-disable no-console */
   console.log(`Listening: http://localhost:${port}`);
   /* eslint-enable no-console */
