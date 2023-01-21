f41905dad5d29f43029f92775fe64c9360cacb6d
kazi naeem rayhan <kazinaeemrayhan@gmail.com>
2023-01-21 19:18:46 +0600

    user model added

--- src/resources/user/user.interface.ts
+++ src/resources/user/user.interface.ts
@@ -2,9 +2,13 @@ import { Document } from 'mongoose';
 
 export default interface User extends Document {
   email: string;
-  name: string;
+  first_name: string;
+  last_name: string;
   password: string;
   role: string;
+  phone: string;
+  address: string;
+  order: Array<[]>;
 
   isValidPassword(password: string): Promise<Error | boolean>;
 }
---
+++ src/resources/user/user.model.ts
@@ -0,0 +1,62 @@
+import { Schema, model } from 'mongoose';
+import bcrypt from 'bcrypt';
+import User from './user.interface';
+
+const UserSchema = new Schema(
+    {
+        first_name: {
+            type: String,
+            required: true,
+        },
+        last_name: {
+            type: String,
+            required: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            trim: true,
+        },
+        phone:{
+            type: String,
+            required: true,
+        },
+        password: {
+            type: String,
+            required: true,
+        },
+        role: {
+            type: String,
+            required: true,
+        },
+        address: {
+            type: String,
+        },
+        order: [{
+            type: Schema.Types.ObjectId,
+            ref: "Order",
+        }]
+    },
+    { timestamps: true }
+);
+
+UserSchema.pre<User>('save', async function (next) {
+    if (!this.isModified('password')) {
+        return next();
+    }
+
+    const hash = await bcrypt.hash(this.password, 10);
+
+    this.password = hash;
+
+    next();
+});
+
+UserSchema.methods.isValidPassword = async function (
+    password: string
+): Promise<Error | boolean> {
+    return await bcrypt.compare(password, this.password);
+};
+
+export default model<User>('User', UserSchema);
