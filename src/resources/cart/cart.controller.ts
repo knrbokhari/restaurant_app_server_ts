b922567b2c2335cd2a2c0f6ff58e48315d63853a
kazi naeem rayhan <kazinaeemrayhan@gmail.com>
2023-01-27 16:49:11 +0600

    decreaseCartProduct controller added

--- src/resources/cart/cart.controller.ts
+++ src/resources/cart/cart.controller.ts
@@ -32,7 +32,7 @@ class OrderController implements Controller{
         );
         this.router.put(`${this.path}/decrease-cart/:id`, 
         authenticatedMiddleware, 
-        // this.decreaseCartProduct
+        this.decreaseCartProduct
         );
     }
 
@@ -135,5 +135,29 @@ class OrderController implements Controller{
     };
 
     // decrease Cart quantity
+    private decreaseCartProduct = async (
+        req: Request,
+        res: Response,
+        next: NextFunction
+    ): Promise<Response | void> => {
+        try {
+            const { id } = req.params;
+
+            const preCart = await this.CartService.findCartById( id );
+
+            if ( !preCart ) throw new Error("Product not found");
+
+            await this.CartService.decreaseCartProductQuantity(id);
+
+            const user = await this.UserService.getAUser(preCart.clientId);
+
+            res.status(200).json({
+                success: true,
+                user,
+              });
+        } catch (error: any) {
+            next(new HttpException(400, error.message));
+        }
+    };
 
 }
