87a4a30ee14b1e37d576a036a846fc6598b74c14
kazi naeem rayhan <kazinaeemrayhan@gmail.com>
2023-02-09 22:31:00 +0600

    permission model added

--- src/resources/permission/permission.interface.ts
+++ src/resources/permission/permission.interface.ts
@@ -2,11 +2,7 @@ import { Document } from 'mongoose';
 
 export default interface Permission extends Document {
     roleId: string;
-    roleName: string;
-    roleAlias: string;
     resourceId: string;
-    resourceName: string;
-    resourceAlias: string;
     isAllowed: string;
     type: string;
 }
