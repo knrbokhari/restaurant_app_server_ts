6d02e12d5dfce8a7e9f8bf3711f5b9859b9b8a63
kazi naeem rayhan <kazinaeemrayhan@gmail.com>
2023-02-11 15:54:11 +0600

    Review model added

---
+++ src/resources/review/review.model.ts
@@ -0,0 +1,27 @@
+import { Schema, model } from 'mongoose';
+import Review from './review.interface';
+
+const ReviewSchema = new Schema(
+    {
+        name: {
+            type: String,
+            require: true
+        },
+        email: {
+            type: String,
+            require: true        
+        },
+        review: {
+            type: String,
+            require: true        
+        },
+        rating: {
+            type: String,
+            require: true        
+        }
+    },
+    { timestamps: true }
+);
+
+
+export default model<Review>('Review', ReviewSchema);
