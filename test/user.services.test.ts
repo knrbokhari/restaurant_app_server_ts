fa2efd09a855536350975f6f1b55cd1f0d23e30e
kazi naeem rayhan <kazinaeemrayhan@gmail.com>
2023-03-04 20:02:53 +0600

    user.services.test.ts added

---
+++ test/user.services.test.ts
@@ -0,0 +1,9 @@
+import App from "../src/app";
+import request from "supertest";
+import UserController from "../src/resources/user/user.controller";
+
+describe("User Controller Test Suite", () => {
+    test('should first', () => { 
+        console.log('user test')
+     })
+});
